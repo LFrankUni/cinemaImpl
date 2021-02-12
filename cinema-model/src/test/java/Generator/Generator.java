e613b8a438d3580f4ff71119699b6dafd87eb9f7
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-02-12 09:05:22 +0100

    Generator Test: Create Fresh Object.

--- cinema-model/src/test/java/Generator/Generator.java
+++ cinema-model/src/test/java/Generator/Generator.java
@@ -1,12 +1,30 @@
 package Generator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+import db.executer.PersistenceException;
+import generated.test.Class_1;
+import generated.test.Class_2;
+
+@TestInstance(Lifecycle.PER_CLASS)
 public class Generator {
-	
+
+	@BeforeAll
+	public void init() {
+		generated.test.Test.getInstance();
+	}
+
 	@Test
 	public void testConnection() {
 		generated.test.Test.getInstance();
 	}
 
+	@Test
+	public void testFreshCreate() throws PersistenceException {
+		Class_1.createFresh(Class_2.createFresh(), true);
+	}
+	
 }
