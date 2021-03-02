5c34c23f40248d2bdff92f053e8bd276398bb9e5
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-03-02 10:43:45 +0100

    Model -> Ticket State Available Impl

--- cinema-model/src/generated/cinemaService/Available.java
+++ cinema-model/src/generated/cinemaService/Available.java
@@ -3,6 +3,7 @@
  * --- Do not touch section numbering!   
  */
 package generated.cinemaService;
+
 //10 ===== GENERATED:      Import Section =========
 import java.sql.SQLException;
 import db.connection.NoConnectionException;
@@ -15,60 +16,71 @@ import exceptions.ConstraintViolation;
 //20 ===== Editable : Your Import Section =========
 
 //25 ===== GENERATED:      Header Section =========
-public class Available extends TicketState implements java.io.Serializable, IAvailable
-{
-   //30 ===== GENERATED:      Attribute Section ======
-   //40 ===== Editable : Your Attribute Section ======
-   
-   //50 ===== GENERATED:      Constructor ============
-   private Available(Integer id, Ticket ticket, boolean objectOnly)
-   throws ConstraintViolation, PersistenceException{
-      super(id, ticket, objectOnly);
-      if(objectOnly) return;
-   }
-   /** Caution: A Call to this Method Requires to add any newly instantiated Object to its Cache! */
-   public static Available createAlreadyPersistent(AvailableProxy proxy, Ticket ticket)throws ConstraintViolation, PersistenceException{
-      if(proxy.isObjectPresent()) return proxy.getTheObject();
-      return new Available(proxy.getId(), ticket, true);
-   }
-   public static Available createFresh(Ticket ticket)throws ConstraintViolation, PersistenceException{
-      db.executer.DBDMLExecuter dmlExecuter = PersistenceExecuterFactory.getConfiguredFactory().getDBDMLExecuter();
-      Integer id = dmlExecuter.getNextId();
-      try{
-         dmlExecuter.insertInto("TicketAction", "id, typeKey", 
-         id.toString() + ", " + TypeKeyManager.getTheInstance().getTypeKey("CinemaService", "Available").toString());
-      }catch(SQLException|NoConnectionException e){throw new PersistenceException(e.getMessage());}
-      Available me = new Available(id, ticket, false);
-      CinemaService.getInstance().addAvailableProxy(new AvailableProxy(me));
-      return me;
-   }
-   //60 ===== Editable : Your Constructors ===========
-   
-   //70 ===== GENERATED:      Feature Access =========
-   public Available getTheObject(){
-      return this;
-   }
-   //80 ===== Editable : Your Operations =============
-/**
- * Unreserves the ticket if possible.
- */
-   public Ticket unreserve()throws ModelException{
-      // TODO: Implement Operation unreserve
-      return null;
-   }
-/**
- * Reserves the ticket if possible.
- */
-   public Ticket reserve(User user)throws ModelException{
-      // TODO: Implement Operation reserve
-      return null;
-   }
-/**
- * Books the ticket if possible.
- */
-   public Ticket book()throws ModelException{
-      // TODO: Implement Operation book
-      return null;
-   }
+public class Available extends TicketState implements java.io.Serializable, IAvailable {
+	// 30 ===== GENERATED: Attribute Section ======
+	// 40 ===== Editable : Your Attribute Section ======
+
+	// 50 ===== GENERATED: Constructor ============
+	private Available(Integer id, Ticket ticket, boolean objectOnly) throws ConstraintViolation, PersistenceException {
+		super(id, ticket, objectOnly);
+		if (objectOnly)
+			return;
+	}
+
+	/**
+	 * Caution: A Call to this Method Requires to add any newly instantiated Object
+	 * to its Cache!
+	 */
+	public static Available createAlreadyPersistent(AvailableProxy proxy, Ticket ticket)
+			throws ConstraintViolation, PersistenceException {
+		if (proxy.isObjectPresent())
+			return proxy.getTheObject();
+		return new Available(proxy.getId(), ticket, true);
+	}
+
+	public static Available createFresh(Ticket ticket) throws ConstraintViolation, PersistenceException {
+		db.executer.DBDMLExecuter dmlExecuter = PersistenceExecuterFactory.getConfiguredFactory().getDBDMLExecuter();
+		Integer id = dmlExecuter.getNextId();
+		try {
+			dmlExecuter.insertInto("TicketAction", "id, typeKey", id.toString() + ", "
+					+ TypeKeyManager.getTheInstance().getTypeKey("CinemaService", "Available").toString());
+		} catch (SQLException | NoConnectionException e) {
+			throw new PersistenceException(e.getMessage());
+		}
+		Available me = new Available(id, ticket, false);
+		CinemaService.getInstance().addAvailableProxy(new AvailableProxy(me));
+		return me;
+	}
+	// 60 ===== Editable : Your Constructors ===========
+
+	// 70 ===== GENERATED: Feature Access =========
+	public Available getTheObject() {
+		return this;
+	}
+
+	// 80 ===== Editable : Your Operations =============
+	/**
+	 * Unreserves the ticket if possible.
+	 */
+	public Ticket unreserve() throws ModelException {
+		throw new ModelException("Must not unreserve an available Ticket");
+	}
+
+	/**
+	 * Reserves the ticket if possible.
+	 */
+	public Ticket reserve(User user) throws ModelException {
+		// TODO MPS user optional
+		// this.getTicket().setUser(user)
+		this.getTicket().setState(Reserved.createFresh(this.getTicket()));
+		return this.getTicket();
+	}
+
+	/**
+	 * Books the ticket if possible.
+	 */
+	public Ticket book() throws ModelException {
+		throw new ModelException("Must not booked an available Ticket");
+	}
 //90 ===== GENERATED: End of Your Operations ======
 }
