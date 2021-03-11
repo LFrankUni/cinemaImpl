02de43d7b4d6aa2d0de9e66008e3ac22e14ec495
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-03-11 19:24:57 +0100

    UI -> Locale DE

--- cinema-web-ui-angular/src/app/core/core.module.ts
+++ cinema-web-ui-angular/src/app/core/core.module.ts
@@ -1,9 +1,19 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import { registerLocaleData } from '@angular/common';
+import localeDE from '@angular/common/locales/de';
+
+registerLocaleData(localeDE);
 @NgModule({
   exports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'de',
+    },
+  ],
 })
 export class CoreModule {}
