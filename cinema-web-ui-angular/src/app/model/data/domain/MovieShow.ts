f392abc15ab2c35619a20b6aaeab1954b0bc2677
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-03-13 11:48:40 +0100

    UI -> Model -> Show: Removed Movie

--- cinema-web-ui-angular/src/app/model/data/domain/MovieShow.ts
+++ cinema-web-ui-angular/src/app/model/data/domain/MovieShow.ts
@@ -1,10 +1,8 @@
 import { Identifiable } from '../Identifiable';
-import { Movie } from './Movie';
 
 export interface MovieShow extends Identifiable {
   start: string;
   end: string;
   threeDimensional: boolean;
   priceInCent: number;
-  movie?: Movie;
 }
