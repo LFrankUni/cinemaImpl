2a385272929758cf202bc9fcb92693772a30ce39
Leonard Frank <leonard.frank@edu.fhdw.de>
2021-03-28 19:28:24 +0200

    TicketTest angepasst

--- cinema-model/src/test/java/TicketTest.java
+++ cinema-model/src/test/java/TicketTest.java
@@ -6,34 +6,18 @@ import org.junit.jupiter.api.Test;
 
 import db.executer.PersistenceException;
 import exceptions.ConstraintViolation;
-import generated.cinemaService.Booked;
-import generated.cinemaService.Cinema;
+import generated.cinemaService.Available;
 import generated.cinemaService.ModelException;
-import generated.cinemaService.Movie;
-import generated.cinemaService.MovieShow;
-import generated.cinemaService.Room;
-import generated.cinemaService.RoomRow;
-import generated.cinemaService.RowCategory;
-import generated.cinemaService.Seat;
-import generated.cinemaService.Ticket;
 
 class TicketTest {
 
 	@Test
 	void ticketUnreservedToReveservedTest() throws PersistenceException, ConstraintViolation {
-		//Setup von anderen Objekten zum Aufbauen des Tickets
-		RowCategory testLoge = RowCategory.createFresh("Loge", 300);
-		RoomRow testRow = RoomRow.createFresh(testLoge, "Saal 1");
-		Seat testSeat = Seat.createFresh(2, testRow);
-		Cinema testCinema = Cinema.createFresh("Test Cinema");
-		Room testRoom = Room.createFresh("Saal 1", true, testCinema);
-		Movie testMovie = Movie.createFresh("Test Film", "Einfach nur zum Testen", 90);
-		MovieShow testShow = MovieShow.createFresh(testMovie, "2021-10-03 20:00:00", "2021-10-03 21:30:00", false, 500, testRoom);
-		Ticket testTicket = Ticket.createFresh(testSeat, testShow);
+		Available availableState = Available.createFresh();
 		
-		//Ticket sollte nicht gebucht werden können, wenn es nicht schon im State reserved ist
+		//Im AvailableState sollte ein Ticket nicht gebucht werden können
 		assertThrows(ModelException.class, () -> {
-			testTicket.setState(Booked.createFresh());
+			availableState.book();
 		});
 	}
 
